d8cbe75379d18c2ca47fa869f38b95d4c2700407
Fernando <fmonteroperez1@sheffield.ac.uk>
2018-03-13 20:48:03 +0000

    fucking mongodb

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var mongodb = require('mongodb');
-
-// router.get('/', function(req, res, next) {
-//     res.render('index', { title: 'Express' });
-// });
+var MongoClient = mongodb.MongoClient;
+// Define where the MongoDB server is
+var url = 'mongodb://localhost:27017/intelligentWeb';
 
 router.get('/', function(req, res, next) {
-    var MongoClient = mongodb.MongoClient;
+    res.render('index', { title: 'Express' });
+});
 
-    // Define where the MongoDB server is
-    var url = 'mongodb://localhost:27017/intelligentWeb';
+router.get('/restaurantList', function(req, res, next) {
 
     // Connect to the server
     MongoClient.connect(url, function (err, db) {
@@ -25,27 +24,24 @@ router.get('/', function(req, res, next) {
             var collection = dbo.collection('resturants');
 
             findDocuments(collection);
+            db.close();
         }
     });
 
     function findDocuments(col) {
-// Find all students
         col.find({}).toArray(function (err, result) {
             if (err) {
                 res.send(err);
             } else if (result.length) {
                 res.render('resturants',{
-
-                    // Pass the returned database documents to Jade
                     "resturantlist" : result
                 });
             } else {
                 res.send('No documents found');
             }
-            //Close connection
-            db.close();
         })
     };
+
 });
 
 
